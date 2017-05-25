05d05ec8e8ad2a0643ce7015982a1f9c994680f0
Tom Parker <tparker@usgs.gov>
2017-05-25 08:54:23 -0800

    style tweaks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,13 +31,13 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.doctest',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.imgmath',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -158,6 +158,3 @@ texinfo_documents = [
      author, 'tomputils', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
