de2fff0d85995241bfa5b27a0312eb815bb51b5c
tparker <tparker@usgs.gov>
2017-05-31 09:59:07 -0800

    flake8 fixes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,10 +23,12 @@ import sphinx_rtd_theme
 import sys
 from mock import Mock as MagicMock
 
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
             return MagicMock()
+
 
 MOCK_MODULES = ['pycurl']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
