fce30aeeca0d20c5261aa59782f11b9401decb87
Tom Parker <tparker@usgs.gov>
2018-02-26 14:30:36 -0900

    init message

---
+++ tomputils/message/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Purpose: Interface with a RabbitMQ message broker
+#   Author: Tom Parker
+#
+# -----------------------------------------------------------------------------
+"""
+tomputils.message
+====================
+
+Interface with a RabbitMQ message broker.
+
+:license:
+    CC0 1.0 Universal
+    http://creativecommons.org/publicdomain/zero/1.0/
+"""
+
+from message import Alarm
+
+__all__ = ['Alarm']
