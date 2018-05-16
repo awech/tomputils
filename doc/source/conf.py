b7bfbdd1ecd0e77bf06fea2c24e290692109d1e3
tparker <tparker@usgs.gov>
2018-05-16 15:52:45 -0800

    tweak mocked modules for RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
             return MagicMock()
 
 
-MOCK_MODULES = ['pycurl']
+MOCK_MODULES = ['pycurl', 'pyOpenSSL']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
