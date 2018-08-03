fe80213e90cd54505127d055c09be03f8f6aface
tparker <tparker@usgs.gov>
2018-08-02 20:22:15 -0800

    add util package

---
+++ tomputils/util/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Purpose: utility functions
+#   Author: Tom Parker
+#
+# -----------------------------------------------------------------------------
+"""
+tomputils.util
+====================
+
+A few utility functions.
+
+:license:
+    CC0 1.0 Universal
+    http://creativecommons.org/publicdomain/zero/1.0/
+"""
+
