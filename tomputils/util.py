997d6d9723e3d87bb93fe0b818949b6e62eefdca
tparker <tparker@usgs.gov>
2018-08-05 12:12:09 -0800

    fix log format

--- tomputils/util.py
+++ tomputils/util.py
@@ -118,7 +118,7 @@ def setup_logging(subject="Error logs"):
     logger.setLevel(logging.DEBUG)
 
     ch = logging.StreamHandler()
-    fmt = "%(levelname)s - %(message)s (%(asctime)s %(filename)s:%(lineno)s)"
+    fmt = "%(asctime)s %(levelname)s - %(message)s (%(filename)s:%(lineno)s)"
     formatter = logging.Formatter(fmt)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
